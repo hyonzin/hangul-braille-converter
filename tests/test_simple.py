aeacfe9f2ec2419d17ebfbbab0790008f68a42ff
hyonzin <hyeonjin507@gmail.com>
2017-12-09 04:15:01 +0900

    convert hangul to braille

--- tests/test_simple.py
+++
@@ -1,8 +0,0 @@
-# the inclusion of the tests module is not meant to offer best practices for
-# testing in general, but rather to support the `find_packages` example in
-# setup.py that excludes installing the "tests" package
-
-import hbcvt
-
-def test_success():
-    assert hbcvt.h2b.return0() == 0
